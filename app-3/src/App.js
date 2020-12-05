8254b57d8ec87d0682a6710743f93161f0c39036
Alex Romo III <alexromo3rd@gmail.com>
2020-12-05 13:23:49 -0800

    app-3 complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filterString: '',
+      randomArray: ['This', 'is', 'just', 'some', 'random', 'string', 'array']
+    }
+  }
+
+  handleChange(value) {this.setState({
+      filterString: value
+    })
+  }
+
+  render() {
+    const filteredStringArray = this.state.randomArray.filter((element) => {
+      return (
+        element.includes(this.state.filterString)
+      )
+    }).map((element, index) => {
+      return (
+        <h2 key={index}>{element}</h2>
+      )
+    })
+    
+    return (
+      <div className="App">
+        <input onChange={e => {this.handleChange(e.target.value)}} />
+        {filteredStringArray}
+      </div>
+    );
+  }
 }
 
 export default App;
