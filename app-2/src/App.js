54a9e8425983a01ac8c8bf6437fc44869182105f
Alex Romo III <alexromo3rd@gmail.com>
2020-12-05 12:59:41 -0800

    app-2 complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      randomArray: ['This', 'is', 'just', 'some', 'random', 'string', 'array']
+    }
+  }
+
+  render() {
+    const displayArray = this.state.randomArray.map(element => {
+      return(
+        <h2>{element}</h2>
+      )
+    })
+
+    return (
+      <div className="App">
+        {displayArray}    
+      </div>
+    );
+  }
 }
 
 export default App;
